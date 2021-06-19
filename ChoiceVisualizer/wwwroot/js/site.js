b3c843330a869294044b2000e541345b7cfcae51
mekk1t <kit.kalimov@gmail.com>
2021-06-19 13:15:20 +0300

    fix

--- ChoiceVisualizer/wwwroot/js/site.js
+++ ChoiceVisualizer/wwwroot/js/site.js
@@ -3,9 +3,4 @@
 
 // Write your JavaScript code.
 
-function fixCarouselItems() {
-    var activeCarouselItems = document.querySelectorAll('div.carousel-item.active');
-    for (let i = 1; i < activeCarouselItems.length; i++) {
-        activeCarouselItems[i].classList.remove('active');
-    }
-}
+
