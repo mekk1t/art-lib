9e3c349ae506254253a5064b3def8743003f5da4
mekk1t <kit.kalimov@gmail.com>
2021-06-19 13:09:31 +0300

    Верстка полуфинала и финалов

--- ChoiceVisualizer/wwwroot/js/site.js
+++ ChoiceVisualizer/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function fixCarouselItems() {
+    var activeCarouselItems = document.querySelectorAll('div.carousel-item.active');
+    for (let i = 1; i < activeCarouselItems.length; i++) {
+        activeCarouselItems[i].classList.remove('active');
+    }
+}
