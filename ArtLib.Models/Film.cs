4f074da37721981c2bce72b6e63e17876a0487b3
mekk1t <kit.kalimov@gmail.com>
2021-07-03 21:27:51 +0300

    Update Film.cs

--- ArtLib.Models/Film.cs
+++ ArtLib.Models/Film.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArtLib.Models
 {
@@ -7,7 +8,7 @@ namespace ArtLib.Models
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Director { get; set; }
-        public string Genre { get; set; }
+        public IEnumerable<string> Genres { get; set; }
         public TimeSpan Duration { get; set; }
         public string Poster { get; set; }
     }
