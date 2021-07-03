cc937259e0956c67bb2d7c4d536251509e9b1fb7
mekk1t <kit.kalimov@gmail.com>
2021-07-03 22:09:45 +0300

    Update AppDbContext.cs

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ArtLib.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Database
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<DbFilm> Films { get; set; }
-        public DbSet<DbGame> Games { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Genre> Genres { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
