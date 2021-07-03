ee967d7f750341eaef1edfc99f3bdfb6e91bb9ec
mekk1t <kit.kalimov@gmail.com>
2021-07-03 21:26:47 +0300

    Класс для описания моделей и их взаимосвязей

---
+++ ArtLib.Models/Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArtLib.Models
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Poster { get; set; }
+    }
+}
---
+++ ArtLib.Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArtLib.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public IEnumerable<string> Genres { get; set; }
+        public string Publisher { get; set; }
+        public string Developer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public TimeSpan HoursPlayed { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsReplayable { get; set; }
+    }
+}
