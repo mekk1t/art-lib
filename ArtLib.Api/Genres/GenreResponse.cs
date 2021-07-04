8702b1108c1f2a75bc69350a4f9d62e79fad1708
mekk1t <kit.kalimov@gmail.com>
2021-07-04 23:27:26 +0300

    Модель ответа о жанре

---
+++ ArtLib.Api/Genres/GenreResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace KitProjects.ArtLib.Api.Genres
+{
+    /// <summary>
+    /// Информация о жанре.
+    /// </summary>
+    public class GenreResponse
+    {
+        /// <summary>
+        /// ID жанра в числовом формате.
+        /// </summary>
+        public long Id { get; }
+        /// <summary>
+        /// Название жанра.
+        /// </summary>
+        public string Name { get; }
+
+        public GenreResponse(long id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
