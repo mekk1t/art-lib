0725457ca1e18f61d8a6c9e9b26d3117bfa378e5
mekk1t <kit.kalimov@gmail.com>
2021-07-04 23:27:44 +0300

    Регистрация зависимостей в контейнере

--- ArtLib.Api/Startup.cs
+++ ArtLib.Api/Startup.cs
@@ -1,6 +1,12 @@
-﻿using KitProjects.Api.AspNetCore.Extensions;
+﻿using Database;
+using KitProjects.Api.AspNetCore.Extensions;
+using KitProjects.ArtLib.Core;
+using KitProjects.ArtLib.Core.Abstractions;
+using KitProjects.ArtLib.Core.Models;
+using KitProjects.ArtLib.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,15 +14,24 @@ namespace KitProjects.ArtLib.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(_configuration.GetConnectionString("Database"));
+            });
+
+            services.AddScoped<ICrud<Game, QueryArgsBase>, GamesCrud>();
+            services.AddScoped<ICrud<Genre, QueryArgsBase>, GenresCrud>();
+            services.AddScoped<GamesService>();
+            services.AddScoped<GenresService>();
+
             services.AddApiCore(serializeEnumsAsStrings: true);
             services.AddSwaggerV1("ArtLib");
         }
